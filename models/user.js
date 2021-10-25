7b140ae2228def2a472a18e91d31088a3bb469d7
Dmytro Petrenko <blinker182ca@gmail.com>
2021-10-25 18:44:03 +0300

    add subscription for user model

--- models/user.js
+++ models/user.js
@@ -17,11 +17,11 @@ const userSchema = Schema(
       required: [true, "Email is required"],
       unique: true,
     },
-    // subscription: {
-    //   type: String,
-    //   enum: ["starter", "pro", "business"],
-    //   default: "starter",
-    // },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
     token: {
       type: String,
       default: null,
