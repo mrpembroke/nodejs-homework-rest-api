f98d83e4f685254754694df7bd85c8aa8b305176
Dmytro Petrenko <blinker182ca@gmail.com>
2021-11-01 00:08:58 +0200

    add reVerification controller

---
+++ controllers/auth/reVerification.js
@@ -0,0 +1,22 @@
+const { User } = require("../../models");
+const { BadRequest } = require("http-errors");
+const { sendEmail } = require("../../utils");
+
+const reVerification = async (req, res) => {
+  const user = await User.findOne({ email });
+  const { verify, verifyToken } = user;
+
+  if (verify) {
+    throw new BadRequest("Verification has already been passed");
+  } else {
+    const data = {
+      to: email,
+      subject: "Подтверждение регистрации",
+      html: `<a href="http://localhost:3000/api/auth/users/verify/${verifyToken}" target="_blank">Подтвердить почту</a>`,
+    };
+
+    await sendEmail(data);
+  }
+};
+
+module.exports = reVerification;
